c416664909f1963f7906c2eac1d5a511700341a4
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2020-01-11 21:17:43 -0700

    Update generate-username.sh
    updated to be more concise

--- generate-username.sh
+++ generate-username.sh
@@ -21,13 +21,13 @@ IFS=$'\n\t'
 generate-username(){
   minLength="${1:-9}"
   maxLength="${1:-16}"
-  length=$(shuf -n1 -i $minLength-$maxLength)
-  wLength=$((length-2))
-  w=$(shuf -er -n$wLength {a..z} {0..9} | tr -d '\n')
-  d=$(shuf -er -n1 {0..9} | tr -d '\n')
-  z=$(echo ${d}${w} | fold -w1 | shuf | tr -d '\n')
-  a=$(shuf -er -n1 {a..z} | tr -d '\n')
-  echo ${a}${z}
+  length=`shuf -n1 -i ${minLength}-${maxLength}`
+  wLength="$((length-2))"
+  w=`shuf -er -n${wLength} {a..z} {0..9}`
+  d=`shuf -er -n1 {0..9}`
+  z=`echo "${d}${w}" | fold -w1 | shuf`
+  a=`shuf -er -n1 {a..z}`
+  echo "$(echo "${a}${z}" | tr -d "\n")"
 }
 
 
