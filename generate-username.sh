afe66ebc8dd03a7d38ef3c8f2d2e1a0aaf20bda4
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2020-01-11 20:55:26 -0700

    Update generate-username.sh
    adjusted spacing

--- generate-username.sh
+++ generate-username.sh
@@ -22,7 +22,7 @@ generate-username(){
   minLength="${1:-9}"
   maxLength="${1:-16}"
   length=$(shuf -n1 -i $minLength-$maxLength)
-  wLength=$(( length - 2))
+  wLength=$((length-2))
   w=$(shuf -er -n$wLength {a..z} {0..9} | tr -d '\n')
   d=$(shuf -er -n1 {0..9} | tr -d '\n')
   z=$(echo ${d}${w} | fold -w1 | shuf | tr -d '\n')
