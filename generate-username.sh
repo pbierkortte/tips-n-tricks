a7a7103c4745a41ca31fd7479431a05092f31a59
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2020-01-13 17:16:16 -0800

    Updated generate-username.sh
    Changed to only skip letter characters

--- generate-username.sh
+++ generate-username.sh
@@ -14,7 +14,7 @@ IFS=$'\n\t'
 #    * Fixed length if argument specified
 #    * No repeated characters, so max length of 32
 #    * Contain only digits and lowercase letters
-#    * Avoid ambiguous characters "1","2","l","o"
+#    * Avoid ambiguous characters "i","l","o","z"
 #
 #  Test Examples:
 #    $ seq 10 | xargs -Iz -P8 ./generate-username.sh   | sort
@@ -27,13 +27,14 @@ IFS=$'\n\t'
 generate-username(){
   minLength="${1:-9}"
   maxLength="${1:-16}"
+  avoid="iloz"
   length=`shuf -n1 -i ${minLength}-${maxLength}`
   wLength="$((length-2))"
-  digit=`echo -n {0..9} | tr -d '12 ' | fold -w1`
-  alpha=`echo -n {a..z} | tr -d 'lo ' | fold -w1`
+  digit=`echo -n {0..9} | tr -d '$avoid ' | fold -w1`
+  alpha=`echo -n {a..z} | tr -d '$avoid ' | fold -w1`
   d=`shuf -e -n1 ${digit}`
   a=`shuf -e -n1 ${alpha}`
-  word=`echo -n {0..9} {a..z} | tr -d "12lo$d$a " | fold -w1`
+  word=`echo -n {0..9} {a..z} | tr -d "$avoid$d$a " | fold -w1`
   w=`shuf -e -n${wLength} ${word}`
   z=`echo "${d}${w}" | fold -w1 | shuf`
   echo "$(echo "${a}${z}" | tr -d "\n")"
