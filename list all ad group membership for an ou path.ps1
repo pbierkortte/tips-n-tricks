7de6fde70849b1d558bcdeecab6f52594bdfff94
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2021-03-15 12:21:11 -0700

    Update list all ad group membership for an ou path.ps1
    Completely removed the SearchBase stub. Removed comments

--- list all ad group membership for an ou path.ps1
+++ list all ad group membership for an ou path.ps1
@@ -1,7 +1,4 @@
-# you can run this in powershell ISE, provided you have the ADUC tools installed
-# replace the seachbase as needed
-
-get-adgroup -Filter { Name -like "*" } -SearchBase "DC=ad,DC=<myDC>,DC=com" | foreach {
+get-adgroup -Filter { Name -like "*" } | foreach {
     write-host $_.name
     get-adgroupmember $_ | foreach {
     $myGroup = ($_ | select -expandproperty samaccountname)
