48b5b7033452777a6e6ccb3d6060c808bb129a73
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2021-03-15 12:18:40 -0700

    Update list all ad group membership for an ou path.ps1
    Name searches are more likely. Added a stub for Name like searches. Removed the SearchBase stub.

--- list all ad group membership for an ou path.ps1
+++ list all ad group membership for an ou path.ps1
@@ -1,7 +1,7 @@
 # you can run this in powershell ISE, provided you have the ADUC tools installed
 # replace the seachbase as needed
 
-get-adgroup -filter * -SearchBase "OU=Roles,OU=<myOrg> Groups,DC=ad,DC=<myDC>,DC=com" | foreach {
+get-adgroup -Filter { Name -like "*" } -SearchBase "DC=ad,DC=<myDC>,DC=com" | foreach {
     write-host $_.name
     get-adgroupmember $_ | foreach {
     $myGroup = ($_ | select -expandproperty samaccountname)
