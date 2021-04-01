12a9c4c9ac30c0b35c70d38a17866469f09ca969
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2021-03-31 23:41:04 -0700

    Create get-local-admins.ps1

---
+++ get-local-admins.ps1
@@ -0,0 +1,19 @@
+$myList = "COMPUTER-1","COMPUTER-2","COMPUTER-3"
+$separator = "*" * 80
+
+[Environment]::UserName
+Get-Date
+
+foreach ($computerName in $myList) {   
+    Write-Output $separator
+    [ADSI]$search = "WinNT://$computerName"
+    $search.children.where({$_.class -eq 'group' -and $_.Name -eq 'Administrators'}) |
+        Select `
+            @{Name="ComputerName";Expression={$_.Parent.split("/")[-1] }},
+            @{Name="GroupName";Expression={$_.name.value}},
+            @{Name="Members";Expression={  
+                [ADSI]$group = "$($_.Parent)/$($_.Name),group"
+                $members = $Group.psbase.Invoke("Members") 
+                $members | % {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
+                $members -join "; "}}
+}
