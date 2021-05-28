8bed7f08acf38d56b0608fde3e06648fb3cc54c8
pbierkortte <24831708+pbierkortte@users.noreply.github.com>
2021-05-28 08:17:35 -0700

    Update get-local-admins.ps1
    Edited to show all members instead of "..."

--- get-local-admins.ps1
+++ get-local-admins.ps1
@@ -1,19 +1,24 @@
 $myList = "COMPUTER-1","COMPUTER-2","COMPUTER-3"
 $separator = "*" * 80
-
 [Environment]::UserName
 Get-Date
 
-foreach ($computerName in $myList) {   
+foreach ($computername in $myList) {   
     Write-Output $separator
-    [ADSI]$search = "WinNT://$computerName"
+    
+    [ADSI]$search  = "WinNT://$computername"
+    
     $search.children.where({$_.class -eq 'group' -and $_.Name -eq 'Administrators'}) |
-        Select `
-            @{Name="ComputerName";Expression={$_.Parent.split("/")[-1] }},
-            @{Name="GroupName";Expression={$_.name.value}},
-            @{Name="Members";Expression={  
-                [ADSI]$group = "$($_.Parent)/$($_.Name),group"
-                $members = $Group.psbase.Invoke("Members") 
-                $members | % {$_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)}
-                $members -join "; "}}
+    
+    Select `
+        @{Name="Computername";Expression={$_.Parent.split("/")[-1] }},
+        @{Name="GroupName";Expression={$_.name.value}},
+        @{Name="Members";Expression={
+            [ADSI]$group = "$($_.Parent)/$($_.Name),group"
+            $members = $Group.psbase.Invoke("Members")
+                ($members | ForEach-Object {
+                $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
+                }) -join "; "
+    }}
+
 }
